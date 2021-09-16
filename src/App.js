c5c1e5bac27906f4fea1844a6326b71a41a7f6b4
Emmanuel <EmmanuelRodriguesBoo@gmail.com>
2021-09-16 02:18:52 -0300

    Exercicio Finalizado

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.componentDidUpdate = this.componentDidUpdate.bind(this);
+    this.dogFetch = this.dogFetch.bind(this);
+    this.state = {
+      message: '',
+      status: true,
+      breed: '',
+    };
+  }
+
+  componentDidMount() {
+    this.dogFetch();
+  }
+
+  componentDidUpdate() {
+    const { message, breed } = this.state;
+    console.log(breed);
+    return (
+      <div>
+        {
+          message.includes('terrier') === true
+            ? this.dogFetch()
+            : <img alt="dog" src={ message } />
+        }
+      </div>
+    );
+  }
+
+  dogFetch() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            message: result.message,
+            status: false,
+            breed: this,
+          });
+        },
+      );
+    const { message } = this.state;
+    localStorage.setItem('prevDog', message);
+  }
+
+  render() {
+    const { status } = this.state;
+    const loadingElement = <span>Loading...</span>;
+    return (
+      <div className="app">
+        <button type="button" onClick={ this.dogFetch }>New Dog</button>
+        { status ? loadingElement : this.componentDidUpdate() }
+        <p>{ localStorage.getItem('prevDog') }</p>
+      </div>
+    );
+  }
 }
 
 export default App;
